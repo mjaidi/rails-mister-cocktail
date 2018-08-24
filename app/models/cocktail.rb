9cd9ab4437c42c1855e254b06d9d9ec73d4a3c3a
Majid Jaidi <majid.jaidi@gmail.com>
2018-08-24 13:48:01 +0100

    added image upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :delete_all
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  mount_uploader :image, ImageUploader
+
 end
